9be968471acb0255b23664eb059d5e0a19460147
Vampaire <natabitrix@ya.ru>
2023-06-07 06:55:00 +0300

    animate home page categories

--- components/bitrix/catalog.section.list/home-page_categories/template.php
+++ components/bitrix/catalog.section.list/home-page_categories/template.php
@@ -21,12 +21,46 @@ $arSectionDeleteParams = array("CONFIRM" => GetMessage('CT_BCSL_ELEMENT_DELETE_C
 <?if(0 < $arResult["SECTIONS_COUNT"]):?>
 
 <?
-$i = 0;
+$i = 1;
 foreach ($arResult['SECTIONS'] as &$arSection):
 	$this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], $strSectionEdit);
 	$this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], $strSectionDelete, $arSectionDeleteParams);
+	$class = "";
+	$animation_type = "";
+	$animation_delay = "";
+	$animation_translate_ratio = "--hs-translate-ratio: 5;";
+	$animation_blur = "--hs-blur: 0px;";
+
+
+	if($i == 1) 
+	{
+		$class = "col-8 offset-2";
+		$animation_type = "fade down zoom-in";
+		$animation_delay = "--hs-delay: 0ms;";
+	}
+	if($i == 2) 
+	{
+		$class = "col-6";
+		$animation_type = "fade right zoom-in";
+		$animation_delay = "--hs-delay: 0ms;";
+	}
+	if($i == 3) 
+	{
+		$class = "col-6";
+		$animation_type = "fade left zoom-in";
+		$animation_delay = "--hs-delay: 0ms;";
+	}
+	if($i == 4) 
+	{
+		$class = "col-8 offset-2";
+		$animation_type = "fade up zoom-in";
+		$animation_delay = "--hs-delay: 0ms;";
+	}
+	$animation_delay = "";
+	$animation_blur = "";
+
 ?>
-	<div class="home-section__categories-item <?if($i == 0 || $i == 3):?>col-8 offset-2<?else:?>col-6<?endif?>" data-hs="fade up">
+	<div class="home-section__categories-item <?=$class?> anim-repeat" data-hs="<?=$animation_type?>" style="<?=$animation_delay?><?=$animation_translate_ratio?><?=$animation_blur?>">
 		<a href="<?=$arSection["SECTION_PAGE_URL"]?>">
 		<?if($arSection["UF_LOGO"]):?> 
 		<img src="<?=CFile::GetPath($arSection["UF_LOGO"])?>" alt="<?=$arSection["NAME"];?>" title="<?=$arSection["NAME"];?>">
